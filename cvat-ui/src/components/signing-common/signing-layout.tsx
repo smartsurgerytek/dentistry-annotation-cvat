273e68f282f2376955958bd275057f97c9ac1d59
Eason Li <eason.li@smartsurgerytek.com>
2024-11-25 17:44:23 +0800

    Modify Login Page Title to iDental

--- cvat-ui/src/components/signing-common/signing-layout.tsx
+++ cvat-ui/src/components/signing-common/signing-layout.tsx
@@ -81,8 +81,7 @@ function SignInLayout(props: SignInLayoutComponentProps): JSX.Element {
                 <Content>
                     <Row justify='center' align='middle' style={{ height: '100%' }}>
                         <Col {...titleSizes} className='cvat-signing-title'>
-                            <Title>Open Data</Title>
-                            <Title>Annotation Platform</Title>
+                            <Title>iDental</Title>
                         </Col>
                         {children}
                     </Row>
